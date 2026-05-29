5350c0b7b8cf5d682504864035e6daf2995451b4
matthewfro181818 <froncekmatthew173@gmail.com>
2026-05-29 13:39:02 -0400

    Update ScriptedSubtitle.hx

--- source/play/subtitle/ScriptedSubtitle.hx
+++ source/play/subtitle/ScriptedSubtitle.hx
@@ -3,30 +3,28 @@ package play.subtitle;
 import data.subtitle.SubtitleData;
 import polymod.hscript.HScriptedClass;
 
-/**
- * Scriptable subtitle class.
- *
- * SubtitleManager calls ScriptedSubtitle.scriptInit(scriptClass, data, manager).
- * Some Polymod/HScript setups do not auto-generate that static helper, so this
- * file provides a safe fallback.
- */
 @:hscriptClass
-class ScriptedSubtitle extends Subtitle implements HScriptedClass
+class ScriptedSubtitle extends play.subtitle.Subtitle implements HScriptedClass
 {
-	public function new(data:SubtitleData, manager:SubtitleManager)
-	{
-		super(data, manager);
-	}
-
 	public static function scriptInit(scriptClass:Dynamic, data:SubtitleData, manager:SubtitleManager):Subtitle
 	{
 		if (scriptClass != null)
 		{
 			try
 			{
-				var created:Dynamic = Type.createInstance(scriptClass, [data, manager]);
-				if (Std.isOfType(created, Subtitle))
-					return cast created;
+				var cls:Class<Dynamic> = null;
+
+				if (Std.isOfType(scriptClass, String))
+					cls = Type.resolveClass(cast scriptClass);
+				else
+					cls = cast scriptClass;
+
+				if (cls != null)
+				{
+					var created:Dynamic = Type.createInstance(cls, [data, manager]);
+					if (Std.isOfType(created, Subtitle))
+						return cast created;
+				}
 			}
 			catch (e:Dynamic)
 			{
